31a974c5699c8f1d3d99d496dd8fe1ef1bdceddc
David Socha <David1Socha@gmail.com>
2014-11-26 18:45:44 -0600

    Fixed flawed expectation in add test

--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -11,7 +11,7 @@ describe "Assembler" do
 
     it "correctly converts add instructions into hex" do
       hex_form = @assembler.convert_hex ["Add", "r2", "r0", "r4"]
-      expect(hex_form).to eq("20460A")
+      expect(hex_form).to eq("20460C")
     end
 
   end
