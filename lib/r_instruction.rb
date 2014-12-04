9e0b2f42055cd876a696c316290bbc3a0639d2db
David Socha <David1Socha@gmail.com>
2014-12-04 02:19:37 -0600

    Added overriding #== method to RInstruction

--- lib/r_instruction.rb
+++ lib/r_instruction.rb
@@ -3,7 +3,7 @@ require_relative './codes'
 module Assembly
   
   class RInstruction
-    attr_accessor :cond, :opcode, :regT, :regD, :regS, :opx, :S, :command
+    attr_accessor :cond, :opcode, :regT, :regD, :regS, :opx, :s, :command
 
     def initialize(command)
       @command = command
@@ -19,6 +19,18 @@ module Assembly
 
     def to_hex
       return "000000"
+    end
+
+    def ==(other)
+      same_cond = @cond == other.cond
+      same_opcode = @opcode == other.opcode
+      same_regT = @regT == other.regT
+      same_regD = @regD == other.regD
+      same_regS = @regS == other.regS
+      same_opx = @opx == other.opx
+      same_s = @s == other.s
+      same_command = @command == other.command
+      return same_command && same_s && same_opx && same_opcode && same_cond && same_regS && same_regD && same_regT
     end
 
   end
