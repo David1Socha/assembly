aaa9505481d434a05168810dbcf454b2466b9261
David Socha <David1Socha@gmail.com>
2014-12-07 12:14:10 -0600

    Added generic #convert_hex #convert_binary tests to helper

--- spec/assembler_spec_helper.rb
+++ spec/assembler_spec_helper.rb
@@ -45,3 +45,13 @@ end
 def get_jr_tokens
   ["JR", "R3"]
 end
+
+def test_convert_hex(assembler, tokens, expected_hex_form)
+  hex_form = assembler.convert_hex tokens
+  expect(hex_form).to eq(expected_hex_form)
+end
+
+def test_convert_binary(asssembler, tokens, expected_binary_form)
+  binary_form = assembler.convert_binary tokens
+  expect(binary_form).to eq(expected_binary_form)
+end
