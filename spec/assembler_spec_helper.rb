f4f6aa2fef9589a5f28436ed1aba0ba97ec576a6
David Socha <David1Socha@gmail.com>
2014-12-07 12:19:02 -0600

    Fixed typo in argument name for #test_convert_binary

--- spec/assembler_spec_helper.rb
+++ spec/assembler_spec_helper.rb
@@ -51,7 +51,7 @@ def test_convert_hex(assembler, tokens, expected_hex_form)
   expect(hex_form).to eq(expected_hex_form)
 end
 
-def test_convert_binary(asssembler, tokens, expected_binary_form)
+def test_convert_binary(assembler, tokens, expected_binary_form)
   binary_form = assembler.convert_binary tokens
   expect(binary_form).to eq(expected_binary_form)
 end
