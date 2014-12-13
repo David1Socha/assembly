a86a440f2be488c41e7c3898f653c93a609c65fc
David Socha <David1Socha@gmail.com>
2014-12-13 15:35:55 -0600

    Changed assembler_spec to not expect newlines in hex/binary conversions

--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -64,35 +64,35 @@ describe "Assembler" do
     describe "(R type)" do
 
       it "correctly converts add tokens into binary" do
-        test_convert_binary(@assembler, get_add_tokens, "001000000100011000001100\n")
+        test_convert_binary(@assembler, get_add_tokens, "001000000100011000001100")
       end
 
       it "correctly converts sub tokens into binary" do
-        test_convert_binary(@assembler, get_sub_tokens, "001000100010100000001100\n")
+        test_convert_binary(@assembler, get_sub_tokens, "001000100010100000001100")
       end
 
       it "correctly converts and tokens into binary" do
-        test_convert_binary(@assembler, get_and_tokens, "010101111001000000001100\n")
+        test_convert_binary(@assembler, get_and_tokens, "010101111001000000001100")
       end
 
       it "correctly converts xor tokens into binary" do
-        test_convert_binary(@assembler, get_xor_tokens, "010101111001010000001100\n")
+        test_convert_binary(@assembler, get_xor_tokens, "010101111001010000001100")
       end
 
       it "correctly converts or tokens into binary" do
-        test_convert_binary(@assembler, get_or_tokens, "010101111001001000001100\n")
+        test_convert_binary(@assembler, get_or_tokens, "010101111001001000001100")
       end
 
       it "correctly converts sll tokens into binary" do
-        test_convert_binary(@assembler, get_sll_tokens, "010000110010000000001111\n")
+        test_convert_binary(@assembler, get_sll_tokens, "010000110010000000001111")
       end
 
       it "correctly converts cmp tokens into binary" do
-        test_convert_binary(@assembler, get_cmp_tokens, "011100000000000100001110\n")
+        test_convert_binary(@assembler, get_cmp_tokens, "011100000000000100001110")
       end
 
       it "correctly converts jr tokens into binary" do
-        test_convert_binary(@assembler, get_jr_tokens, "000000110000000000001101\n")
+        test_convert_binary(@assembler, get_jr_tokens, "000000110000000000001101")
       end
       
     end
@@ -100,15 +100,15 @@ describe "Assembler" do
     describe "(J type)" do
 
       it "correctly converts jump tokens into binary" do
-        test_convert_binary(@assembler, get_jump_tokens, "000000000000001110000000\n")
+        test_convert_binary(@assembler, get_jump_tokens, "000000000000001110000000")
       end
 
       it "correctly converts jump and link tokens into binary" do
-        test_convert_binary(@assembler, get_jal_tokens, "000000000000001110000001\n")
+        test_convert_binary(@assembler, get_jal_tokens, "000000000000001110000001")
       end
 
       it "correctly converts load immediate tokens into binary" do
-        test_convert_binary(@assembler, get_loadi_tokens, "010000000000000001010010\n")
+        test_convert_binary(@assembler, get_loadi_tokens, "010000000000000001010010")
       end
 
     end
@@ -117,15 +117,15 @@ describe "Assembler" do
     describe "(D type)" do
 
       it "correctly converts addi tokens into binary" do
-        test_convert_binary(@assembler, get_addi_tokens, "001101001110010000001010\n")
+        test_convert_binary(@assembler, get_addi_tokens, "001101001110010000001010")
       end
 
       it "correctly converts load word tokens into binary" do
-        test_convert_binary(@assembler, get_lw_tokens, "001001101111100000001000\n")
+        test_convert_binary(@assembler, get_lw_tokens, "001001101111100000001000")
       end
 
       it "correctly converts store word tokens into binary" do
-        test_convert_binary(@assembler, get_sw_tokens, "001001101111100000001001\n")
+        test_convert_binary(@assembler, get_sw_tokens, "001001101111100000001001")
       end
 
     end
@@ -133,11 +133,11 @@ describe "Assembler" do
     describe "(B type)" do
 
       it "correctly converts branch tokens into binary" do
-        test_convert_binary(@assembler, get_b_tokens, "000000000000111100000100\n")
+        test_convert_binary(@assembler, get_b_tokens, "000000000000111100000100")
       end
 
       it "correctly converts branch and link tokens into binary" do
-        test_convert_binary(@assembler, get_bal_tokens, "000000000000111100000101\n")
+        test_convert_binary(@assembler, get_bal_tokens, "000000000000111100000101")
       end
 
     end
@@ -145,71 +145,71 @@ describe "Assembler" do
     describe "(Instruction Extensions)" do
 
       it "correctly adds S bit in binary" do
-        test_convert_binary(@assembler, get_adds_tokens, "001000000100011100001100\n")
+        test_convert_binary(@assembler, get_adds_tokens, "001000000100011100001100")
       end
 
       it "correctly adds AL cond in binary" do
-        test_convert_binary(@assembler, get_addal_tokens, "001000000100011000001100\n")
+        test_convert_binary(@assembler, get_addal_tokens, "001000000100011000001100")
       end
 
       it "correctly adds NV cond in binary" do
-        test_convert_binary(@assembler, get_addnv_tokens, "001000000100011000011100\n")
+        test_convert_binary(@assembler, get_addnv_tokens, "001000000100011000011100")
       end
 
       it "correctly adds EQ cond in binary" do
-        test_convert_binary(@assembler, get_addeq_tokens, "001000000100011000101100\n")
+        test_convert_binary(@assembler, get_addeq_tokens, "001000000100011000101100")
       end
 
       it "correctly adds NE cond in binary" do
-        test_convert_binary(@assembler, get_addne_tokens, "001000000100011000111100\n")
+        test_convert_binary(@assembler, get_addne_tokens, "001000000100011000111100")
       end
 
       it "correctly adds VS cond in binary" do
-        test_convert_binary(@assembler, get_addvs_tokens, "001000000100011001001100\n")
+        test_convert_binary(@assembler, get_addvs_tokens, "001000000100011001001100")
       end
 
       it "correctly adds VC cond in binary" do
-        test_convert_binary(@assembler, get_addvc_tokens, "001000000100011001011100\n")
+        test_convert_binary(@assembler, get_addvc_tokens, "001000000100011001011100")
       end
 
       it "correctly adds MI cond in binary" do
-        test_convert_binary(@assembler, get_addmi_tokens, "001000000100011001101100\n")
+        test_convert_binary(@assembler, get_addmi_tokens, "001000000100011001101100")
       end
 
       it "correctly adds PL cond in binary" do
-        test_convert_binary(@assembler, get_addpl_tokens, "001000000100011001111100\n")
+        test_convert_binary(@assembler, get_addpl_tokens, "001000000100011001111100")
       end
 
       it "correctly adds CS cond in binary" do
-        test_convert_binary(@assembler, get_addcs_tokens, "001000000100011010001100\n")
+        test_convert_binary(@assembler, get_addcs_tokens, "001000000100011010001100")
       end
 
       it "correctly adds CC cond in binary" do
-        test_convert_binary(@assembler, get_addcc_tokens, "001000000100011010011100\n")
+        test_convert_binary(@assembler, get_addcc_tokens, "001000000100011010011100")
       end
 
       it "correctly adds HI cond in binary" do
-        test_convert_binary(@assembler, get_addhi_tokens, "001000000100011010101100\n")
+        test_convert_binary(@assembler, get_addhi_tokens, "001000000100011010101100")
       end
 
       it "correctly adds LS cond in binary" do
-        test_convert_binary(@assembler, get_addls_tokens, "001000000100011010111100\n")
+        test_convert_binary(@assembler, get_addls_tokens, "001000000100011010111100")
       end
 
       it "correctly adds GT cond in binary" do
-        test_convert_binary(@assembler, get_addgt_tokens, "001000000100011011001100\n")
+        test_convert_binary(@assembler, get_addgt_tokens, "001000000100011011001100")
       end
 
       it "correctly adds LT cond in binary" do
-        test_convert_binary(@assembler, get_addlt_tokens, "001000000100011011011100\n")
+        test_convert_binary(@assembler, get_addlt_tokens, "001000000100011011011100")
       end
 
       it "correctly adds GE cond in binary" do
-        test_convert_binary(@assembler, get_addge_tokens, "001000000100011011101100\n")
+        test_convert_binary(@assembler, get_addge_tokens, "001000000100011011101100")
       end
 
       it "correctly adds LE cond in binary" do
-        test_convert_binary(@assembler, get_addle_tokens, "001000000100011011111100\n")
+        test_convert_binary(@assembler, get_addle_tokens, "001000000100011011111100")
       end
 
     end
@@ -222,7 +222,7 @@ describe "Assembler" do
       assembler = Assembly::Assembler.new ""
       expect(assembler).to receive(:tokenize_lines)
       expect(assembler).to receive(:tokenized_lines).and_return([get_add_tokens,get_jump_tokens])
-      expect(assembler.return_mif).to eq "WIDTH=24;\nDEPTH=1024;\n\nADDRESS_RADIX=UNS;\nDATA_RADIX=HEX;\n\nCONTENT BEGIN\n20460c\n000380\nEND;\n"
+      expect(assembler.return_mif).to eq "WIDTH=24;\nDEPTH=1024;\n\nADDRESS_RADIX=UNS;\nDATA_RADIX=HEX;\n\nCONTENT BEGIN\n0 : 20460C;\n1 : 000380;\n[2..1023] : 000000;\nEND;\n"
     end
 
   end
